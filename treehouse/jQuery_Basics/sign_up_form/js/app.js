43cd441d75f9434e288810a112e21fd1851e2995
Raven Dunn <ravendunn0807@gmail.com>
2016-05-15 14:54:14 -0400

    Upload sign_up_form project.

---
+++ treehouse/jQuery_Basics/sign_up_form/js/app.js
@@ -0,0 +1,5 @@
+//Problem: Hints are shown even when form is valid
+//Solution: Hide and show them at appropriate times
+//Hide hints
+//When event happens on password input
+//When event happens on confirmation input
