d0616bb06ae20b1630d613483996932003c392ee
Raven Dunn <ravendunn0807@gmail.com>
2016-05-24 15:56:38 -0400

    Upload project in videos folder.

--- treehouse/introducing-jquery-plugins/using_jquery_plugins/section2/videos/js/main.js
+++ treehouse/introducing-jquery-plugins/using_jquery_plugins/section2/videos/js/main.js
@@ -6,3 +6,12 @@ $(".animsition").animsition({
   outDuration: 500
 });
 
+$('.header').sticky();
+
+$('.header').on('sticky-start', function () {
+  $('.description').html('We build <strong>great</strong> apps');
+});
+
+$('.header').on('sticky-end', function () {
+  $('.description').html('We build apps');
+});
